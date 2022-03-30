7ada6fbcd60d0fc6e898eaebb2d36491ddb412a6
huwarr <mikhalmatova@gmail.com>
2022-03-30 23:22:27 +0300

    fixed "gdown" issue

--- setup.sh
+++ setup.sh
@@ -2,11 +2,13 @@
 conda create -n tat-qa python==3.7
 conda activate tat-qa
 pip install -r requirement.txt
-pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.10.0+cu102.html  #+${CUDA}.html
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.10.0+cu+${CUDA}.html
 
 # Download pretrained GenBERT (out_syntext_and_numeric_finetune_numeric)
 cd tag_op
+pip install --upgrade --no-cache-dir gdown
 gdown --folder https://drive.google.com/drive/folders/1-KmhWF4Jex4gyuz1J2VANd1ycmtsggQ3
+cd ..
 
 # Prepare dataset
 PYTHONPATH=$PYTHONPATH:$(pwd):$(pwd)/tag_op python tag_op/prepare_dataset.py --mode [train/dev/test]
